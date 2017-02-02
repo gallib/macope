f80dcf4d141f8f82414f5cc2aa9ae5b9f6f9d3f7
Alain Pellaux <alain@gallib.net>
2017-02-02 06:26:01 +0100

    add an add link

--- src/resources/views/accounts/index.blade.php
+++ src/resources/views/accounts/index.blade.php
@@ -9,40 +9,49 @@
     </div>
     <div class="row">
         <div class="col-md-12">
+            @include('macope::helpers.form-message')
             <div class="panel panel-default">
-                @include('macope::helpers.form-message')
                 <div class="panel-body">
-                    <table class="table table-bordered table-condensed" id="account-table">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Description</th>
-                                <th>Iban</th>
-                                <th>Currency</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($accounts as $account)
-                            <tr>
-                                <td>
-                                    <a href="{{ route('accounts.show', $account->id) }}" title="View details">{{ $account->name }}</a>
-                                </td>
-                                <td>{{ $account->description }}</td>
-                                <td>{{ $account->iban }}</td>
-                                <td>{{ $account->currency }}</td>
-                                <td>
-                                    <a class="btn btn-primary" href="{{ route('accounts.edit', $account->id) }}">Edit</a>
-                                    @if ($account->journalEntries()->count() === 0)
-                                        {{ Form::open(['method' => 'DELETE','route' => ['accounts.destroy', $account->id], 'style'=>'display:inline']) }}
-                                        {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
-                                        {{ Form::close() }}
-                                    @endif
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <a href="{{ route('accounts.create') }}" title="Add an account" class="btn btn-primary">Add</a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <table class="table table-bordered table-condensed" id="account-table">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Description</th>
+                                        <th>Iban</th>
+                                        <th>Currency</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach ($accounts as $account)
+                                    <tr>
+                                        <td>
+                                            <a href="{{ route('accounts.show', $account->id) }}" title="View details">{{ $account->name }}</a>
+                                        </td>
+                                        <td>{{ $account->description }}</td>
+                                        <td>{{ $account->iban }}</td>
+                                        <td>{{ $account->currency }}</td>
+                                        <td>
+                                            <a class="btn btn-primary" href="{{ route('accounts.edit', $account->id) }}">Edit</a>
+                                            @if ($account->journalEntries()->count() === 0)
+                                                {{ Form::open(['method' => 'DELETE','route' => ['accounts.destroy', $account->id], 'style'=>'display:inline']) }}
+                                                {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
+                                                {{ Form::close() }}
+                                            @endif
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
