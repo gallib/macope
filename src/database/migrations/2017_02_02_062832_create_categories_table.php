311a86b4d9c03fad4a29f8913d71c6430fb60d26
Alain Pellaux <alain@gallib.net>
2017-02-02 17:08:20 +0100

    add categories, type_categories migrations schema

---
+++ src/database/migrations/2017_02_02_062832_create_categories_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->integer('type_category_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('type_category_id')->references('id')->on('type_categories');
+        });
+
+        Schema::table('journal_entries', function (Blueprint $table) {
+           $table->integer('category_id')->nullable()->unsigned()->after('balance');
+
+           $table->foreign('category_id')->references('id')->on('categories');
+       });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
