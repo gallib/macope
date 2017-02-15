90450a5462bf792e6e8225987ee1f0ad85b2ba9e
Alain Pellaux <alain@gallib.net>
2017-02-15 20:48:40 +0100

    bootstrap 4 style with account information

--- src/resources/views/import-file/index.blade.php
+++ src/resources/views/import-file/index.blade.php
@@ -1,11 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-body">
+        <div class="col">
+            <h1>Import a file</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    <div class="header-block">
+                        Import a file
+                    </div>
+                </div>
+                <div class="card-block">
                     @include('macope::helpers.form-message')
                     {{ Form::open(['url' => route('import-file.import'), 'files' => true]) }}
                     <div class="form-group">
