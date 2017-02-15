7af6c6b7af70b79f1709a1b52df4cf745024e54f
Alain Pellaux <alain@gallib.net>
2017-02-15 22:32:13 +0100

    bootstrap 4 style

--- src/resources/views/categorizations/index.blade.php
+++ src/resources/views/categorizations/index.blade.php
@@ -1,53 +1,57 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col">
             <h1>Categorizations</h1>
         </div>
     </div>
     <div class="row">
-        <div class="col-md-12">
-            @include('macope::helpers.form-message')
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <a href="{{ route('categorizations.create') }}" title="Add a categorization" class="btn btn-primary">Add</a>
-                        </div>
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    <div class="header-block">
+                        Categorizations
+                        <a href="{{ route('categorizations.create') }}" title="Add a categorization" class="btn btn-primary btn-sm pull-right">Add</a>
                     </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <table class="table table-bordered table-condensed" id="categorizations-table">
-                                <thead>
-                                    <tr>
-                                        <th>Search</th>
-                                        <th>Type</th>
-                                        <th>Category</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                @foreach ($categorizations as $categorization)
-                                    <tr>
-                                        <td>
-                                            <a href="{{ route('categorizations.show', $categorization->id) }}" title="View details">{{ $categorization->search }}</a>
-                                        </td>
-                                        <td>{{ $categorization->type }}</td>
-                                        <td>{{ $categorization->category->name }}</td>
-                                        <td>
-                                            <a class="btn btn-primary" href="{{ route('categorizations.edit', $categorization->id) }}">Edit</a>
-                                            {{ Form::open(['method' => 'DELETE','route' => ['categorizations.destroy', $categorization->id], 'style'=>'display:inline']) }}
-                                            {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
-                                            {{ Form::close() }}
-                                        </td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                </div>
+                <div class="card-block">
+                    @include('macope::helpers.form-message')
+                    <table class="table table-bordered table-condensed" id="categorizations-table">
+                        <thead>
+                            <tr>
+                                <th>Search</th>
+                                <th>Type</th>
+                                <th>Category</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($categorizations as $categorization)
+                            <tr>
+                                <td>
+                                    <a href="{{ route('categorizations.show', $categorization->id) }}" title="View details">{{ $categorization->search }}</a>
+                                </td>
+                                <td>{{ $categorization->type }}</td>
+                                <td>{{ $categorization->category->name }}</td>
+                                <td>
+                                    <a href="{{ route('categorizations.show', $categorization->id) }}" title="View details">
+                                        <i class="fa fa-eye"></i>
+                                    </a>
+                                    <a href="{{ route('categorizations.edit', $categorization->id) }}" title="Edit">
+                                        <i class="fa fa-pencil"></i>
+                                    </a>
+                                    {{ Form::open(['method' => 'DELETE','route' => ['categorizations.destroy', $categorization->id], 'style'=>'display:inline']) }}
+                                        <button class="macope-delete" type="submit">
+                                            <i class="fa fa-trash"></i>
+                                        </button>
+                                    {{ Form::close() }}
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -59,7 +63,10 @@
 <script>
 $(function() {
     $('#categorizations-table').DataTable({
-        pageLength: 25
+        pageLength: 25,
+        columnDefs: [
+            {orderable: false, targets: 3}
+        ]
     });
 });
 </script>
