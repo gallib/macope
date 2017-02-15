5072b0195117ee61e0bc7e793c54a3e6c66ee073
Alain Pellaux <alain@gallib.net>
2017-02-15 21:29:11 +0100

    bootstrap 4 style

--- src/resources/views/categories/index.blade.php
+++ src/resources/views/categories/index.blade.php
@@ -1,53 +1,57 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col">
             <h1>Categories</h1>
         </div>
     </div>
     <div class="row">
-        <div class="col-md-12">
-            @include('macope::helpers.form-message')
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <a href="{{ route('categories.create') }}" title="Add a category" class="btn btn-primary">Add</a>
-                        </div>
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    <div class="header-block">
+                        Categories
+                        <a href="{{ route('categories.create') }}" title="Add a category" class="btn btn-primary btn-sm pull-right">Add</a>
                     </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <table class="table table-bordered table-condensed" id="categories-table">
-                                <thead>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Type category</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                @foreach ($categories as $category)
-                                    <tr>
-                                        <td>
-                                            <a href="{{ route('categories.show', $category->id) }}" title="View details">{{ $category->name }}</a>
-                                        </td>
-                                        <td>{{ $category->typeCategory->name }}</td>
-                                        <td>
-                                            <a class="btn btn-primary" href="{{ route('categories.edit', $category->id) }}">Edit</a>
-                                            @if ($category->journalEntries()->count() === 0)
-                                                {{ Form::open(['method' => 'DELETE','route' => ['categories.destroy', $category->id], 'style'=>'display:inline']) }}
-                                                {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
-                                                {{ Form::close() }}
-                                            @endif
-                                        </td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                </div>
+                <div class="card-block">
+                    @include('macope::helpers.form-message')
+                    <table class="table table-bordered table-condensed" id="categories-table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Type category</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($categories as $category)
+                            <tr>
+                                <td>
+                                    <a href="{{ route('categories.show', $category->id) }}" title="View details">{{ $category->name }}</a>
+                                </td>
+                                <td>{{ $category->typeCategory->name }}</td>
+                                <td>
+                                    <a href="{{ route('categories.show', $category->id) }}" title="View details">
+                                        <i class="fa fa-eye"></i>
+                                    </a>
+                                    <a href="{{ route('categories.edit', $category->id) }}" title="Edit">
+                                        <i class="fa fa-pencil"></i>
+                                    </a>
+                                    @if ($category->journalEntries()->count() === 0)
+                                        {{ Form::open(['method' => 'DELETE','route' => ['categories.destroy', $category->id], 'style'=>'display:inline']) }}
+                                            <button class="macope-delete" type="submit">
+                                                <i class="fa fa-trash"></i>
+                                            </button>
+                                        {{ Form::close() }}
+                                    @endif
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -59,7 +63,10 @@
 <script>
 $(function() {
     $('#categories-table').DataTable({
-        pageLength: 25
+        pageLength: 25,
+        columnDefs: [
+            {orderable: false, targets: 2}
+        ]
     });
 });
 </script>
