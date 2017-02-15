9c57ed0b40efbf268db57b4ce3624dd638bc51cc
Alain Pellaux <alain@gallib.net>
2017-02-15 22:23:18 +0100

    bootstrap 4 style

--- src/resources/views/type-categories/show.blade.php
+++ src/resources/views/type-categories/show.blade.php
@@ -1,17 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col">
             <h1>{{ $typeCategory->name }}</h1>
         </div>
     </div>
     <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    {{ $typeCategory->name }}
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    <div class="header-block">
+                        Informations
+                    </div>
+                </div>
+                <div class="card-block">
+                    <p><span class="font-weight-bold">Name: </span>{{ $typeCategory->name }}</p>
                 </div>
             </div>
         </div>
