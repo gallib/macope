0eedf7257215e775af67a4414c53d34cac656fe2
Alain Pellaux <alain@gallib.net>
2017-02-15 22:18:15 +0100

    bootstrap 4 style

--- src/resources/views/type-categories/edit.blade.php
+++ src/resources/views/type-categories/edit.blade.php
@@ -1,16 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col">
             <h1>{{ $typeCategory->name }}</h1>
         </div>
     </div>
     <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-body">
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    <div class="header-block">
+                        {{ $typeCategory->name }}
+                    </div>
+                </div>
+                <div class="card-block">
                     @include('macope::helpers.form-message')
                     {{ Form::model($typeCategory, ['url' => route('type-categories.update', $typeCategory->id), 'method' => 'PUT']) }}
                     <div class="form-group">
