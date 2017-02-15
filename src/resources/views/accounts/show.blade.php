3571a222f001836a780b34641e322f15ed364f97
Alain Pellaux <alain@gallib.net>
2017-02-15 17:06:20 +0100

    bootstrap 4 style with account information

--- src/resources/views/accounts/show.blade.php
+++ src/resources/views/accounts/show.blade.php
@@ -1,16 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col">
             <h1>{{ $account->name }}</h1>
         </div>
     </div>
     <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-body">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    <div class="header-block">
+                        Informations
+                    </div>
+                </div>
+                <div class="card-block">
+                    <p><span class="font-weight-bold">Description: </span>{{ $account->description }}</p>
+                    <p><span class="font-weight-bold">Iban: </span>{{ $account->iban }}</p>
+                    <p><span class="font-weight-bold">Currency: </span>{{ $account->currency }}</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <div class="header-block">
+                        Balances
+                    </div>
+                </div>
+                <div class="card-block">
                     <table class="table table-bordered table-condensed" id="monthly-balances-table">
                         <thead>
                             <tr>
