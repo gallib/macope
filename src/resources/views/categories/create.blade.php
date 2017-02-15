c866a872bafd55d14b81b6b73df24bd02081181a
Alain Pellaux <alain@gallib.net>
2017-02-15 21:38:41 +0100

    bootstrap 4 style

--- src/resources/views/categories/create.blade.php
+++ src/resources/views/categories/create.blade.php
@@ -1,16 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col">
             <h1>Create a category</h1>
         </div>
     </div>
     <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-body">
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    <div class="header-block">
+                        Create a category
+                    </div>
+                </div>
+                <div class="card-block">
                     @include('macope::helpers.form-message')
                     {{ Form::open(['url' => route('categories.store')]) }}
                     <div class="form-group">
