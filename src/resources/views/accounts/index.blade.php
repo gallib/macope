506e000bf223b043a244ae4b2b40f4e967dee4d5
Alain Pellaux <alain@gallib.net>
2017-02-15 06:37:09 +0100

    bootstrap 4 style

--- src/resources/views/accounts/index.blade.php
+++ src/resources/views/accounts/index.blade.php
@@ -1,57 +1,59 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col">
             <h1>Accounts</h1>
         </div>
     </div>
     <div class="row">
-        <div class="col-md-12">
-            @include('macope::helpers.form-message')
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <a href="{{ route('accounts.create') }}" title="Add an account" class="btn btn-primary">Add</a>
-                        </div>
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    <div class="header-block">
+                        Accounts
+                        <a href="{{ route('accounts.create') }}" title="Add an account" class="btn btn-primary btn-sm pull-right">Add</a>
                     </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <table class="table table-bordered table-condensed" id="account-table">
-                                <thead>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Description</th>
-                                        <th>Iban</th>
-                                        <th>Currency</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                @foreach ($accounts as $account)
-                                    <tr>
-                                        <td>
-                                            <a href="{{ route('accounts.show', $account->id) }}" title="View details">{{ $account->name }}</a>
-                                        </td>
-                                        <td>{{ $account->description }}</td>
-                                        <td>{{ $account->iban }}</td>
-                                        <td>{{ $account->currency }}</td>
-                                        <td>
-                                            <a class="btn btn-primary" href="{{ route('accounts.edit', $account->id) }}">Edit</a>
-                                            @if ($account->journalEntries()->count() === 0)
-                                                {{ Form::open(['method' => 'DELETE','route' => ['accounts.destroy', $account->id], 'style'=>'display:inline']) }}
-                                                {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
-                                                {{ Form::close() }}
-                                            @endif
-                                        </td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                </div>
+                <div class="card-block">
+                    @include('macope::helpers.form-message')
+                    <table class="table table-bordered table-condensed" id="account-table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th>Iban</th>
+                                <th>Currency</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($accounts as $account)
+                            <tr>
+                                <td>{{ $account->name }}</td>
+                                <td>{{ $account->description }}</td>
+                                <td>{{ $account->iban }}</td>
+                                <td>{{ $account->currency }}</td>
+                                <td>
+                                    <a href="{{ route('accounts.show', $account->id) }}" title="View details">
+                                        <i class="fa fa-eye"></i>
+                                    </a>
+                                    <a href="{{ route('accounts.edit', $account->id) }}" title="Edit">
+                                        <i class="fa fa-pencil"></i>
+                                    </a>
+                                    @if ($account->journalEntries()->count() === 0)
+                                        {{ Form::open(['method' => 'DELETE','route' => ['accounts.destroy', $account->id], 'style'=>'display:inline']) }}
+                                            <button class="macope-delete" type="submit">
+                                                <i class="fa fa-trash"></i>
+                                            </button>
+                                        {{ Form::close() }}
+                                    @endif
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -63,7 +65,10 @@
 <script>
 $(function() {
     $('#account-table').DataTable({
-        pageLength: 25
+        pageLength: 25,
+        columnDefs: [
+            {orderable: false, targets: 4}
+        ]
     });
 });
 </script>
