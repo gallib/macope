4d235e692f946799d2f1da4a73dd70e4c7191915
Alain Pellaux <alain@gallib.net>
2017-02-16 06:54:10 +0100

    bootstrap 4 style

--- src/resources/views/categorizations/show.blade.php
+++ src/resources/views/categorizations/show.blade.php
@@ -1,19 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col">
             <h1>{{ $categorization->search }}</h1>
         </div>
     </div>
     <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <p>Search: {{ $categorization->search }}</p>
-                    <p>Type: {{ $categorization->type }}</p>
-                    <p>Category: {{ $categorization->category->name }}</p>
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    <div class="header-block">
+                        Informations
+                    </div>
+                </div>
+                <div class="card-block">
+                    <p><span class="font-weight-bold">Search: </span>{{ $categorization->search }}</p>
+                    <p><span class="font-weight-bold">Type: </span>{{ $categorization->type }}</p>
+                    <p><span class="font-weight-bold">Category: </span>{{ $categorization->category->name }}</p>
                 </div>
             </div>
         </div>
