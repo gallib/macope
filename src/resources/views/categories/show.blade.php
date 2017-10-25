1bf02708d3765abe92b450e25c7c71522c51ff84
Alain Pellaux <alain@gallib.net>
2017-10-25 17:05:09 +0200

    add is ignored information

--- src/resources/views/categories/show.blade.php
+++ src/resources/views/categories/show.blade.php
@@ -21,6 +21,7 @@
                         <span class="font-weight-bold">Type category: </span>
                         <a href="{{ route('type-categories.show', $category->typeCategory->id) }}" title="View type category detail">{{ $category->typeCategory->name }}</a>
                     </p>
+                    <p><span class="font-weight-bold">Is ignored: </span>{{ $category->is_ignored ? 'Yes' : 'No' }}</p>
                 </div>
             </div>
         </div>
