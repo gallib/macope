fd33c770f5c439911d6276dc4d82779cc0998c60
Alain Pellaux <alain@gallib.net>
2017-11-15 17:13:36 +0100

    file was default example

--- src/resources/assets/js/components/LastExpenses.vue
+++ src/resources/assets/js/components/LastExpenses.vue
@@ -1,23 +1,65 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
-
-                    <div class="panel-body">
-                        I'm an example component!!
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div>
+        <canvas id="last-expenses"></canvas>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                expenses: []
+            };
+        },
         mounted() {
-            console.log('Component mounted.')
+            this.getExpenses();
+            this.buildChart();
+        },
+        computed: {
+            getDates: function () {
+                var dates = [];
+                this.expenses.forEach(function (data) {
+                    dates.push(data.year + '-' + data.month);
+                });
+                return dates;
+            },
+            getAmounts: function () {
+                var amounts = [];
+                this.expenses.forEach(function (data) {
+                    amounts.push(data.debit);
+                });
+                return amounts;
+            }
+        },
+        methods: {
+            /**
+             * Get latest expenses
+             */
+            getExpenses() {
+                axios
+                    .post('/macope/expenses-last-sum')
+                    .then(response => {
+                        this.expenses = response.data;
+                        this.buildChart();
+                    });
+            },
+            /*
+             * Build the chart
+             */
+            buildChart() {
+                var myBarChart = new Chart(document.getElementById("last-expenses"), {
+                    type: 'bar',
+                    data: {
+                        labels: this.getDates,
+                        datasets: [{
+                            label: 'Expenses',
+                            data: this.getAmounts
+                            backgroundColor: 'rgba(0, 54, 48, 1)',
+                            borderWidth: 1
+                        }]
+                    }
+                });
+            }
         }
     }
 </script>
