b8bb40cd10e347062c98730cbce3357df98b69e8
Alain Pellaux <alain@gallib.net>
2017-11-30 16:53:07 +0100

    update method to retrieve sum for expenses or incomes

--- src/app/Queries/JournalEntryQuery.php
+++ src/app/Queries/JournalEntryQuery.php
@@ -59,16 +59,19 @@ class JournalEntryQuery extends AbstractQuery
     }
 
     /**
-     * Getter sum of last expenses group by month
+     * Get sum of expenses or incomes group by month
      *
+     * @param  string $type
      * @param  \DateTime $dateStart
      * @param  \DateTime $dateEnd
      * @return \Illuminate\Support\Collection
      */
-    public function getLastExpensesSum(DateTime $dateStart = null, DateTime $dateEnd = null)
+    public function getSumByMonth($type, DateTime $dateStart = null, DateTime $dateEnd = null)
     {
+        $type = $type == 'debit' ? 'debit' : 'credit';
+
         $query = \DB::table('journal_entries')
-            ->select(\DB::raw('YEAR(journal_entries.date) as year'), \DB::raw('MONTH(journal_entries.date) as month'), \DB::raw('SUM(journal_entries.debit) as debit'))
+            ->select(\DB::raw('YEAR(journal_entries.date) as year'), \DB::raw('MONTH(journal_entries.date) as month'), \DB::raw('SUM(journal_entries.' . $type . ') as ' . $type))
             ->join('categories', 'categories.id', '=', 'journal_entries.category_id')
             ->where('categories.is_ignored', '=', 0)
             ->groupBy(\DB::raw('YEAR(journal_entries.date) desc, MONTH(journal_entries.date) desc'));
