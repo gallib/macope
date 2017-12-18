bbc92b44fea6cb9353bf82b43f41f0ba07dd0b58
Alain Pellaux <alain@gallib.net>
2017-12-18 07:21:03 +0100

    save hash and orginal values on create

--- src/app/Observers/JournalEntryObserver.php
+++ src/app/Observers/JournalEntryObserver.php
@@ -18,6 +18,10 @@ class JournalEntryObserver
         if (is_null($entry->category_id)) {
             $this->categorize($entry);
         }
+
+        $this
+            ->generateHash($entry)
+            ->saveOriginalValues($entry);
     }
 
     /**
@@ -31,5 +35,31 @@ class JournalEntryObserver
         $categorizer = new Categorizer();
 
         $categorizer->categorize($entry);
+    }
+
+    /**
+     * Generate a unique hash for the given journal entry
+     *
+     * @param  \Gallib\Macope\App\JournalEntry $entry
+     * @return \Gallib\Macope\App\Observers\JournalEntryObserver
+     */
+    protected function generateHash(JournalEntry $entry)
+    {
+        $entry->hash = $entry->generateHash();
+
+        return $this;
+    }
+
+    /**
+     * Save the original values
+     *
+     * @param  \Gallib\Macope\App\JournalEntry $entry
+     * @return \Gallib\Macope\App\Observers\JournalEntryObserver
+     */
+    protected function saveOriginalValues(JournalEntry $entry)
+    {
+        $entry->original_values = $entry->toJson();
+
+        return $this;
     }
 }
