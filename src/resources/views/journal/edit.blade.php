2263b7dd82cc07a3ac73b2044f04eb1982d9bf53
Alain Pellaux <alain@gallib.net>
2017-12-20 17:28:51 +0100

    add new editable fields

--- src/resources/views/journal/edit.blade.php
+++ src/resources/views/journal/edit.blade.php
@@ -19,10 +19,25 @@
                     @include('macope::helpers.form-message')
                     {{ Form::model($journalEntry, ['url' => route('journal.update', $journalEntry->id), 'method' => 'PUT']) }}
                     <div class="form-group">
+                        {{ Form::label('date', 'Date') }}
+                        {{ Form::text('date', null, ['class' => 'form-control']) }}
+                    </div>
+                    <div class="form-group">
+                        {{ Form::label('text', 'Text') }}
+                        {{ Form::textarea('text', null, ['class' => 'form-control']) }}
+                    </div>
+                    <div class="form-group">
                         {{ Form::label('category_id', 'Category') }}
                         {{ Form::select('category_id', $categories, null, ['class' => 'form-control', 'placeholder' => 'No category']) }}
                     </div>
-
+                    <div class="form-group">
+                        {{ Form::label('credit', 'Credit') }}
+                        {{ Form::text('credit', null, ['class' => 'form-control']) }}
+                    </div>
+                    <div class="form-group">
+                        {{ Form::label('debit', 'Debit') }}
+                        {{ Form::text('debit', null, ['class' => 'form-control']) }}
+                    </div>
                     {{ Form::submit('Edit', ['class' => 'btn btn-primary']) }}
                     {{ Form::close() }}
                 </div>
