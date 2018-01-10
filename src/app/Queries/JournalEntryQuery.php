f2b3cb3a9578ad387823e12549ff7f655bd3b343
Alain Pellaux <alain@gallib.net>
2018-01-10 17:00:14 +0100

    end of month could be configured

--- src/app/Queries/JournalEntryQuery.php
+++ src/app/Queries/JournalEntryQuery.php
@@ -26,19 +26,21 @@ class JournalEntryQuery extends AbstractQuery
      */
     public function getYearlyBilling($type = 'debit', $year = null)
     {
+        $monthEndsOn = config('macope.month_ends_on');
+
         $query = \DB::table('journal_entries')
-            ->select('categories.name as category_name', 'type_categories.name as type_category_name', \DB::raw('YEAR(journal_entries.date) as year'), \DB::raw('MONTH(journal_entries.date) as month'), \DB::raw('SUM(journal_entries.credit) as credit'), \DB::raw('SUM(journal_entries.debit) as debit'))
+            ->select('categories.name as category_name', 'type_categories.name as type_category_name', \DB::raw('YEAR(date_add(journal_entries.date, interval (day(last_day(date)) - ' . $monthEndsOn . ') day)) as year'), \DB::raw('MONTH(date_add(date, interval (day(last_day(journal_entries.date)) - ' . $monthEndsOn . ') day)) as month'), \DB::raw('SUM(journal_entries.credit) as credit'), \DB::raw('SUM(journal_entries.debit) as debit'))
             ->join('categories', 'categories.id', '=', 'journal_entries.category_id')
             ->join('type_categories', 'type_categories.id', '=', 'categories.type_category_id')
             ->where('categories.is_ignored', '=', 0)
-            ->groupBy(\DB::raw('type_categories.name, categories.name, YEAR(journal_entries.date), MONTH(journal_entries.date)'));
+            ->groupBy(\DB::raw('type_categories.name, categories.name, year, month'));
 
         if (!is_null($type)) {
             $query->whereNotNull($type);
         }
 
         if (!is_null($year)) {
-            $query->whereYear('journal_entries.date', '=', $year);
+            $query->whereRaw('YEAR(date_add(journal_entries.date, interval (day(last_day(date)) - ' . $monthEndsOn . ') day)) = ?', [$year]);
         }
 
         return $query->get();
@@ -51,8 +53,10 @@ class JournalEntryQuery extends AbstractQuery
      */
     public function getAvailableYears()
     {
+        $monthEndsOn = config('macope.month_ends_on');
+
         $query = \DB::table('journal_entries')
-            ->select(\DB::raw('YEAR(journal_entries.date) as year'))
+            ->select(\DB::raw('YEAR(date_add(journal_entries.date, interval (day(last_day(date)) - ' . $monthEndsOn . ') day)) as year'))
             ->groupBy('year');
 
         return $query->get();
