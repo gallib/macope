ba41ad2c32a281b7f0a407ba663ff00fb08c94eb
Alain Pellaux <alain@gallib.net>
2018-07-10 06:30:43 +0200

    Add default theme

---
+++ resources/views/layouts/main-nav.blade.php
@@ -0,0 +1,67 @@
+<nav class="col-md-3 col-lg-2">
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('dashboard.index') }}">Dashboard</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('expenses.index') }}">Expenses</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('incomes.index') }}">Incomes</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('journal.index') }}">Journal</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('import-file.index') }}">Import file</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('accounts.index') }}">Accounts</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('categories.index') }}">Categories</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('type-categories.index') }}">Type Categories</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('categorizations.index') }}">Categorizations</a>
+        </li>
+        <!--/<li class="nav-item">
+            <a class="nav-link active" href="#">
+                <span data-feather="home"></span>
+                Dashboard
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <span data-feather="file"></span>
+                Orders
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <span data-feather="shopping-cart"></span>
+                Products
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <span data-feather="users"></span>
+                Customers
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <span data-feather="bar-chart-2"></span>
+                Reports
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <span data-feather="layers"></span>
+                Integrations
+            </a>
+        </li>-->
+    </ul>
+</nav>
