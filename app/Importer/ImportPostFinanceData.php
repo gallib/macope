ed548c51cb55ec85de431874e0b744ea25c93e6e
Alain Pellaux <alain@gallib.net>
2018-08-20 06:53:08 +0200

    Use config helper

--- app/Importer/ImportPostFinanceData.php
+++ app/Importer/ImportPostFinanceData.php
@@ -50,8 +50,8 @@ class ImportPostFinanceData implements ImportDataInterface
     public static function isFileValid(UploadedFile $uploadedFile)
     {
         // Get original config
-        $delimiter = \Config::get('excel.csv.delimiter');
-        $heading = \Config::get('excel.import.heading');
+        $delimiter = config('excel.csv.delimiter');
+        $heading = config('excel.import.heading');
 
         // Set config for that file
         \Config::set('excel.csv.delimiter', self::DELIMITER);
