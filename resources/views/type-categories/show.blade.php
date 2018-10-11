687808e14d5f9cd3bb0ec065ed17389b0ef75a80
Alain Pellaux <alain@gallib.net>
2018-10-11 06:26:23 +0200

    Update page layout

--- resources/views/type-categories/show.blade.php
+++ resources/views/type-categories/show.blade.php
@@ -9,21 +9,14 @@
                 <div class="card-body">
                     <h5 class="card-title">Type category details</h5>
                     <p><span class="font-weight-bold">Name: </span>{{ $typeCategory->name }}</p>
+                    <p><span class="font-weight-bold">Related categories:</span>
+                        @foreach ($typeCategory->categories as $category)
+                            <a href="{{ route('categories.show', $category->id) }}"><span class="badge badge-pill badge-primary">{{ $category->name }}</span></a>
+                        @endforeach
+                    </p>
                 </div>
             </div>
         </div>
-        <div class="col-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Related categories</h5>
-                    @foreach ($typeCategory->categories as $category)
-                        <a href="{{ route('categories.show', $category->id) }}"><span class="badge badge-pill badge-primary">{{ $category->name }}</span></a>
-                    @endforeach
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row">
         <div class="col-6">
             <div class="card">
                 <div class="card-body">
