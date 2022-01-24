ce14acce4fa82eb58e33c51c42ed59d2ac62d964
Alain Pellaux <alain@gallib.net>
2022-01-24 14:48:52 +0100

    Laravel 9.x Shift (#98)
    * Move `resources/lang` folder
    
    * Streamline `$commands` property
    
    * Replace deprecated `HEADER_X_FORWARDED_ALL` constant
    
    * Shift core files
    
    * Use Faker methods
    
    * Remove unnecessary `$model` property
    
    * Shift to class based routes
    
    * Shift config files
    
    * Default config files
    
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them and merged your true customizations -
    where ENV variables may not be used.
    
    * Bump Laravel dependencies
    
    * Use `<env>` tags for configuration
    
    `<env>` tags have a lower precedence than system environment variables making it easier to overwrite PHPUnit configuration values in additional environments, such a CI.
    
    Review this blog post for more details on configuration precedence when testing Laravel: https://jasonmccreary.me/articles/laravel-testing-configuration-precedence/
    
    * Adopt anonymous migrations
    
    * Shift cleanup
    
    * Use fork for uniquewith validator
    
    Co-authored-by: Shift <shift@laravelshift.com>

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,13 +19,6 @@ use Illuminate\Support\Str;
 class UserFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = \App\Models\User::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
@@ -33,10 +26,24 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
             'remember_token' => Str::random(10),
         ];
+    }
+
+    /**
+     * Define the model's unverified state.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function unverified()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'email_verified_at' => null,
+            ];
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,54 @@
 <?php
 
-Route::get('/', 'DashboardController@index')->name('home');
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\CategorizationController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\ImportFileController;
+use App\Http\Controllers\IncomeController;
+use App\Http\Controllers\JournalController;
+use App\Http\Controllers\TypeCategoryController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/expenses/{year?}', 'ExpenseController@index')
+Route::get('/', [DashboardController::class, 'index'])->name('home');
+
+Route::get('/expenses/{year?}', [ExpenseController::class, 'index'])
     ->name('expenses.index')
     ->where('year', '[0-9]+');
 
-Route::post('/expenses-sum-by-month', 'ExpenseController@sumByMonth')
+Route::post('/expenses-sum-by-month', [ExpenseController::class, 'sumByMonth'])
     ->name('expenses.sumbymonth');
 
-Route::post('/expenses-by-type-category', 'ExpenseController@expensesByTypeCategory')
+Route::post('/expenses-by-type-category', [ExpenseController::class, 'expensesByTypeCategory'])
     ->name('expenses.by-type-category');
 
-Route::get('/incomes/{year?}', 'IncomeController@index')
+Route::get('/incomes/{year?}', [IncomeController::class, 'index'])
     ->name('incomes.index')
     ->where('year', '[0-9]+');
 
-Route::post('/incomes-sum-by-month', 'IncomeController@sumByMonth')
+Route::post('/incomes-sum-by-month', [IncomeController::class, 'sumByMonth'])
     ->name('incomes.sumbymonth');
 
-Route::get('/import-file', 'ImportFileController@index')->name('import-file.index');
+Route::get('/import-file', [ImportFileController::class, 'index'])->name('import-file.index');
 
-Route::post('/import-file', 'ImportFileController@importFile')->name('import-file.import');
+Route::post('/import-file', [ImportFileController::class, 'importFile'])->name('import-file.import');
 
-Route::resource('/journal', 'JournalController');
+Route::resource('/journal', JournalController::class);
 
-Route::post('/journal/sum-by-month', 'JournalController@sumByMonth')
+Route::post('/journal/sum-by-month', [JournalController::class, 'sumByMonth'])
     ->name('journal.sumbymonth');
 
-Route::resource('accounts', 'AccountController');
+Route::resource('accounts', AccountController::class);
 
-Route::resource('categorizations', 'CategorizationController');
+Route::resource('categorizations', CategorizationController::class);
 
-Route::resource('categories', 'CategoryController');
+Route::resource('categories', CategoryController::class);
 
-Route::resource('type-categories', 'TypeCategoryController');
+Route::resource('type-categories', TypeCategoryController::class);
 
-Route::post('/monthly-expenses-type-category', 'ExpenseController@monthlyExpensesByTypeCategory');
+Route::post('/monthly-expenses-type-category', [ExpenseController::class, 'monthlyExpensesByTypeCategory']);
 
-Route::post('/monthly-expenses-category', 'ExpenseController@monthlyExpensesByCategory');
+Route::post('/monthly-expenses-category', [ExpenseController::class, 'monthlyExpensesByCategory']);
 
 Auth::routes();
